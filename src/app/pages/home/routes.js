76d49826650426870c4e838d16762d98d0e40652
Eder Munhoz <emunhoz@tray.net.br>
2016-11-07 08:57:23 -0200

    nome rota

--- src/app/pages/home/routes.js
+++ src/app/pages/home/routes.js
@@ -1,4 +1,4 @@
-import Main from './components/Main'
+import Main from './index/Main'
 
 export default [
   { path: '/home', component: Main }
--- src/app/pages/plans/routes.js
+++ src/app/pages/plans/routes.js
@@ -1,4 +1,4 @@
-import Main from './components/Main'
+import Main from './index/Main'
 
 export default [
   { path: '/plans', component: Main }
